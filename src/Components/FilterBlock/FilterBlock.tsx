09419ad65791b1ba0fbee78bb2ef721e83ccd102
Viktor Pirogov <Blade-85@inbox.ru>
2024-03-14 22:44:29 +0300

    Nav фильтры выбираются по очереди

--- src/Components/FilterBlock/FilterBlock.tsx
+++ src/Components/FilterBlock/FilterBlock.tsx
@@ -1,38 +1,60 @@
+"use client";
 import styles from "@components/FilterBlock/FilterBlock.module.css";
 import classNames from "classnames";
+import { useState } from "react";
 
 export default function FilterBlock() {
+  const [filterActive, setFilterActive] = useState("");
   return (
     <div className={classNames(styles.centerBlockFilter, styles.filter)}>
       <div className={styles.filterTitle}>Искать по:</div>
       <div
-        className={classNames(
-          styles.filterButton,
-
-          styles.buttonAuthor,
-
-          styles.btnText
-        )}
+        onClick={() => setFilterActive("author")}
+        className={`${
+          filterActive === "author"
+            ? classNames(
+                styles.filterButtonActive,
+                styles.buttonAuthor,
+                styles.btnTextActive
+              )
+            : classNames(
+                styles.filterButton,
+                styles.buttonAuthor,
+                styles.btnText
+              )
+        } `}
       >
         исполнителю
       </div>
       <div
-        className={classNames(
-          styles.filterButton,
-          styles.buttonYear,
-          styles.btnText
-        )}
+        onClick={() => setFilterActive("year")}
+        className={`${
+          filterActive === "year"
+            ? classNames(
+                styles.filterButtonActive,
+                styles.buttonYear,
+                styles.btnTextActive
+              )
+            : classNames(styles.filterButton, styles.buttonYear, styles.btnText)
+        } `}
       >
         году выпуска
       </div>
       <div
-        className={classNames(
-          styles.filterButton,
-
-          styles.buttonGenre,
-
-          styles.btnText
-        )}
+        onClick={() => setFilterActive("genre")}
+        className={`${
+          filterActive === "genre"
+            ? classNames(
+                styles.filterButtonActive,
+                styles.buttonGenre,
+                styles.btnTextActive
+              )
+            : classNames(
+                styles.filterButton,
+                styles.buttonGenre,
+                styles.btnText
+              )
+        } `}
       >
         жанру
       </div>
