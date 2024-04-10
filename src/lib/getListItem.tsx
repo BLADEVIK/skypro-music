de89abe9059aab10e3d10dd683861327ac3a1f47
Viktor Pirogov <Blade-85@inbox.ru>
2024-04-10 15:23:24 +0300

    РЕФАКТОРИНГ

--- src/lib/getListItem.tsx
+++ src/lib/getListItem.tsx
@@ -1,7 +1,7 @@
 import { trackType } from "../types";
 
 type TrackKeys = Pick<trackType, "author" | "genre" | "release_date">;
-// Сортировка
+// Сортировка списков
 export function getListItem(item: keyof TrackKeys, trackList: trackType[]) {
   const listItem: string[] = [];
   trackList?.forEach((track) => {
